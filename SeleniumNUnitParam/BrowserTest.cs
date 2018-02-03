70c190164d69f1ce184e6b60c6bc38931167e094
Senthil V <senar.v@gmail.com>
2018-02-03 16:49:14 +0530

    Update BrowserTest.cs
    testing poll

--- SeleniumNUnitParam/BrowserTest.cs
+++ SeleniumNUnitParam/BrowserTest.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+//testing
 namespace SeleniumNUnitParam
 {
     public class BrowserTest : Hooks
